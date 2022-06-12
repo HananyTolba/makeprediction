b006eb8183fe714a44a10fba47ab4c0bd04d4c68
Hanany Tolba <hanany100@gmail.com>
2022-06-12 10:08:13 +0200

    gitlab-ci.yml

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
+from matplotlib.ft2font import HORIZONTAL
 import makeprediction as mp
+HORIZONTAL = "''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''"
+print(HORIZONTAL.center(80))
 
-print(f'hello from {mp.__name__}: {mp.version.__version__} \U0001F600')
+with open("assets/logo-ascii-art.txt",'r') as f:
+    for line in f:
+        line = line.center(90)
+        print(repr(line.replace("#","@").replace('\n',' ')))
+welcome = f'Welcome to {mp.__name__}: {mp.version.__version__} \U0001F600'  
+print(welcome.center(80))
+print(HORIZONTAL.center(80))
+
